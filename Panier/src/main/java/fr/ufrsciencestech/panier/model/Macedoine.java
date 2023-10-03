05d6004dfeb62b157328e1a38fc20a5dd54d2ccd
Lucas Rembert <it@lucasrembert.fr>
2023-10-03 15:13:27 +0200

    REFACTOR: ToString function of Macedoine

--- Panier/src/main/java/fr/ufrsciencestech/panier/model/Macedoine.java
+++ Panier/src/main/java/fr/ufrsciencestech/panier/model/Macedoine.java
@@ -2,7 +2,7 @@ package fr.ufrsciencestech.panier.model;
 
 import java.util.ArrayList;
 
-public class Macedoine {
+public class Macedoine implements Fruit{
     private ArrayList<Fruit> fruits;
     public Macedoine(ArrayList<Fruit> fruits) {
         this.fruits = fruits;
@@ -10,11 +10,11 @@ public class Macedoine {
 
     @Override
     public String toString() {
-        String result = "";
+        String result = "Macedoine de ";
         for (Fruit f : fruits) {
-            result += f.toString() + "\n";
+            result += f.getClass().getSimpleName() + ", ";
         }
-        return result;
+        return result.substring(0, result.length() - 2) + " a " + getPrix() + " euros";
     }
 
     public boolean isSeedless() {
@@ -24,5 +24,19 @@ public class Macedoine {
             }
         }
         return true;
+    }
+
+    @Override
+    public double getPrix() {
+        double prix = 0;
+        for (Fruit f : fruits) {
+            prix += f.getPrix();
+        }
+        return prix;
+    }
+
+    @Override
+    public String getOrigine() {
+        return "France";
     }
 }
